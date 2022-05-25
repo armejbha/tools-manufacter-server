74ae01679b0016830154a3eb184ec4768aea6041
armejbha <abdurrahim181878@gmil.com>
2022-05-26 00:59:40 +0600

    get all and single data from server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware 
@@ -15,7 +15,20 @@ async function run() {
     try {
         await client.connect();
         const productsCollection = client.db('manufacter').collection('products');
-
+        // get all data 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const curser = productsCollection.find(query);
+            const products = await curser.toArray();
+            res.send(products);
+        })
+        // get single data 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await itemsCollection.findOne(query);
+            res.send(product);
+        })
     }
     finally {
 
